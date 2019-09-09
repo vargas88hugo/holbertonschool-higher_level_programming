2ef200b10481fbf0f2859d7ad4096f66fdf27c2f
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-09 12:46:01 -0500

    [feature] created 9-easter_egg.py

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,23 +1,2 @@
 #!/usr/bin/python3
-s1 = "is better than"
-print("""The Zen of Python, by Tim Peters
-
-Beautiful {} ugly.
-Explicit {} implicit.
-Simple {} complex.
-Complex {} complicated.
-Flat {} nested.
-Sparse {} dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
-Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!""". format(s1))
+import this
