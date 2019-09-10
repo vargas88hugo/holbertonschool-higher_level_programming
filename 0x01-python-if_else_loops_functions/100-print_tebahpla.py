8f06727441545c5f66247216d3ef65744fd84a44
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-10 13:14:21 -0500

    [fix] missing uppercase case

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    print("{}".format(chr(i)), end='')
+    if i % 2 != 0:
+        i = chr(i - 32)
+    else:
+        i = chr(i)
+    print("{}".format(i), end='')
