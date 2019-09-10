ce4e6473bda66c637e128570ec284bc23238cbb0
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-10 12:15:14 -0500

    [fix] there was an error in the ortography

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,8 @@ number = random.randint(-10000, 10000)
 d = (number % 10) if (number >= 0) else (((number * -1) % 10) * -1)
 s = "Last digit of "
 if (d) > 5:
-    print("{}{:d} is {:d} and is greater that 5".format(s, number, d))
+    print("{}{:d} is {:d} and is greater than 5".format(s, number, d))
 elif (d) == 0:
     print("{}{:d} is {:d} and is 0".format(s, number, d))
 else:
-    print("{}{:d} is {:d} and is less that 6 and not 0".format(s, number, d))
+    print("{}{:d} is {:d} and is less than 6 and not 0".format(s, number, d))
