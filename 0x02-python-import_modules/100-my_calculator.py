25dceee1bb1fd519d6d0c9ce0c7e6cad116ed58f
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-12 14:35:05 -0500

    [fix] I didn't used the import functions lol

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     a = int(argv[1])
     b = int(argv[3])
     if argv[2] == '+':
-        print("{} + {} = {}".format(a, b, a + b))
+        print("{} + {} = {}".format(a, b, add(a, b)))
     elif argv[2] == '-':
-        print("{} - {} = {}".format(a, b, a - b))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
     elif argv[2] == '*':
-        print("{} * {} = {}".format(a, b, a * b))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     elif argv[2] == '/':
-        print("{} / {} = {}".format(a, b, a / b))
+        print("{} / {} = {}".format(a, b, div(a, b)))
     elif argv[2] != '+' and argv[2] != '-':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
