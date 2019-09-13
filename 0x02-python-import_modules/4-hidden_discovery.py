1b466997347601d5861acf18730be26ee5d9f554
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-12 19:34:28 -0500

    [fix] I was not itaring in the matrix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in range(0, len(hidden_4)):
-        if hidden_4[i][:2] != "__":
+    for i in dir(hidden_4):
+        if i[:2] != "__":
             print(dir(hidden_4[i]))
