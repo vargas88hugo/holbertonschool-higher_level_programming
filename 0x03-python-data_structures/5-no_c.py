ab2afbabff324255f0e970fb766795052f5f5b2b
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-17 09:15:43 -0500

    [feature] created 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    a = my_string.replace('c', '')
-    a = my_string.replace('C', '')
+    a = my_string[:]
+    a = a.replace('c', '')
+    a = a.replace('C', '')
     return (a)
