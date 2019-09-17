654aff488f02d4d7e0d71713c88e7321de805127
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-17 13:05:24 -0500

    [fix] removed append and now works with iterations

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    a = []
-    for i in my_list[::-1]:
-        if i % 2 == 0:
-            a.append(True)
+    a = my_list[:]
+    i = 0
+    while i < len(a):
+        if a[i] % 2 == 0:
+            a[i] = True
         else:
-            a.append(False)
+            a[i] = False
+        i = i + 1
     return a
