a84e5ba848f78ab29f0052b26b5f013829193215
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-19 17:40:28 +0000

    [fix] missing case

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update(key=value)
+    a = 0
+    for i in a_dictionary:
+        if i == key:
+            a = 1
+            a_dictionary[i] = value
+    if a == 0:
+        a_dictionary.update(key=value)
     return a_dictionary
