92e1bef103c50e5d3450f073c0a1db16abeb263d
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-19 22:55:14 -0500

    [feature] created 12-roman_to_int.py

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -4,25 +4,6 @@
 #include <object.h>
 #include <stdio.h>
 
-void print_python_list(PyObject *p)
-{
-  Py_ssize_t i, j, k;
-  PyObject *Obj;
-
-  i = ((PyVarObject *)(p))->ob_size;
-  j = ((PyListObject *)(p))->allocated;
-
-  printf("[*] Python list info\n");
-  printf("[*] Size of the Python List = %ld\n", i);
-  printf("[*] Allocated = %ld\n", j);
-
-  for (k = 0; k < i; k++)
-    {
-      Obj = ((PyListObject *)(p))->ob_item[k];
-      printf("Element %ld: %s\n", j, (char *)((PyObject *)(Obj))->ob_type->tp_name);
-    }
-}
-
 void print_python_bytes(PyObject *p)
 {
   Py_ssize_t i, j;
@@ -40,7 +21,7 @@ void print_python_bytes(PyObject *p)
     }
   printf("  size: %ld\n", i);
   printf("  trying string: %s\n", str);
-  
+
   if (i > 10)
     i = 10;
   printf("  first %ld bytes:", i);
@@ -50,4 +31,23 @@ void print_python_bytes(PyObject *p)
       printf(" %02x", (unsigned)(unsigned char)str[j]);
     }
   putchar(10);
+}
+
+void print_python_list(PyObject *p)
+{
+  Py_ssize_t i, j, k;
+  PyObject *Obj;
+
+  i = ((PyVarObject *)(p))->ob_size;
+  j = ((PyListObject *)(p))->allocated;
+
+  printf("[*] Python list info\n");
+  printf("[*] Size of the Python List = %ld\n", i);
+  printf("[*] Allocated = %ld\n", j);
+
+  for (k = 0; k < i; k++)
+    {
+      Obj = ((PyListObject *)(p))->ob_item[k];
+      printf("Element %ld: %s\n", j, (char *)((PyObject *)(Obj))->ob_type->tp_name);
+    }
 }
