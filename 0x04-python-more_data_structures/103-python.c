e05110c1e35ae28d175289de58ae2c5af1b8afd8
vargas88hugo@gmail.com <vargas88hugo@gmail.com>
2019-09-20 01:03:53 +0000

    [feature] 103-python.c

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,53 @@
+#include <Python.h>
+#include <bytesobject.h>
+#include <listobject.h>
+#include <object.h>
+#include <stdio.h>
+
+void print_python_list(PyObject *p)
+{
+  Py_ssize_t i, j, k;
+  PyObject *Obj;
+
+  i = ((PyVarObject *)(p))->ob_size;
+  j = ((PyListObject *)(p))->allocated;
+
+  printf("[*] Python list info\n");
+  printf("[*] Size of the Python List = %ld\n", i);
+  printf("[*] Allocated = %ld\n", j);
+
+  for (k = 0; k < i; k++)
+    {
+      Obj = ((PyListObject *)(p))->ob_item[k];
+      printf("Element %ld: %s\n", j, (char *)((PyObject *)(Obj))->ob_type->tp_name);
+    }
+}
+
+void print_python_bytes(PyObject *p)
+{
+  Py_ssize_t i, j;
+  /* PyObject *Obj; */
+  char * str;
+
+  i = ((PyVarObject *)(p))->ob_size;
+  str = ((PyBytesObject *)(p))->ob_sval;
+
+  printf("[.] bytes object info\n");
+  if (str[0] == 1 && (i == 1 || i == 0))
+    {
+      printf("[ERROR] Invalid Bytes Object\n");
+      return;
+    }
+  printf("  size: %ld\n", i);
+  printf("  trying string: %s\n", str);
+  
+  if (i > 10)
+    i = 10;
+  printf("  first %ld bytes:", i);
+
+  for (j = 0; j < i; j++)
+    {
+      printf(" %02x", (unsigned)(unsigned char)str[j]);
+    }
+  putchar(10);
+}
