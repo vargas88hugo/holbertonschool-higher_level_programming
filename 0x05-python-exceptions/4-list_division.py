9b6a475ffbc4037dfd81740bfbb4710009812293
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-30 12:31:56 -0500

    [fix] missing finally instruction

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,19 +3,19 @@ def list_division(my_list_1, my_list_2, list_length):
     i = 0
     a = []
     while i < list_length:
+        j = 0
         try:
-            a.append(my_list_1[i] / my_list_2[i])
+            j = (my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
             print("division by 0")
-            a.append(0)
             pass
         except TypeError:
             print("wrong type")
-            a.append(0)
             pass
         except IndexError:
             print("out of range")
-            a.append(0)
             pass
+        finally:
+            a.append(j)
         i += 1
     return a
