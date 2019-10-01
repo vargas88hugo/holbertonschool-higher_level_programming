a8bc573efb410ee0172d3e15a8519487c147d9c8
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-01 17:32:34 -0500

    [fix] missing case when size is zero

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -63,5 +63,7 @@ class Square:
     def my_print(self):
         for i in range(self.__position[1]):
             print()
+        if self.__size == 0:
+            print()
         for i in range(self.__size):
             print("{}{}".format(" " * self.__position[0], "#" * self.__size))
