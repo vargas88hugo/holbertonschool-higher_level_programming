3725b8b85d2ff37863d4d9c0bb33c6ab37fc2779
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-01 13:23:49 -0500

    [update] added docstrings comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This is a class for define a private instance attribute.
+
+    Attributes:
+       attr1 (size): size of the square.
+    """
     def __init__(self, size):
-        self._size = size
+        """
+        The constructor for Square class.
+
+        Args:
+           param1 (size): size of the square
+        """
+        self.__size = size
