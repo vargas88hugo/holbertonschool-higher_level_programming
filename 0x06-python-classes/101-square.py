9783b32cc3d6277be606ef801bfea300077e58ad
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-01 22:59:11 -0500

    [update] general update

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -43,13 +43,13 @@ class Square:
 
     @size.setter
     def size(self, size):
-        if type(size) == int:
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        else:
             if size < 0:
                 raise ValueError("size must be >= 0")
             else:
                 self.__size = size
-        else:
-            raise TypeError("size must be an integer")
 
     def area(self):
         """
