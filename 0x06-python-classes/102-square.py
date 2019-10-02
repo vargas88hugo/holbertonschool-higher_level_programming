b3e0ef4ded07fa46e67e74e6b1dde3bde4bfda89
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-01 23:15:44 -0500

    [update] general update

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -63,5 +63,17 @@ class Square:
     def __lt__(self, other):
         return self.__size < other.__size
 
+    def __le__(self, other):
+        return self.__size <= other.__size
+
     def __eq__(self, other):
         return self.__size == other.__size
+
+    def __ne__(self, other):
+        return self.__size != other.__size
+
+    def __gt__(self, other):
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        return self.__size >= other.__size
