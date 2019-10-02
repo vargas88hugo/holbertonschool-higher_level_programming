d0db1849209869cf38d034f11f223b0721751708
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-01 23:46:59 -0500

    [fix] one variable was wrong

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,7 +7,7 @@ class MagicClass:
        attrb1
     """
     def __init__(self, radius=0):
-        self.__radius = radius
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
