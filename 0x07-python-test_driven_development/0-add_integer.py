e43089e3bd0d8e16a85a3b4e3d69d8b3332a43c9
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-03 17:57:28 -0500

    [feature] created 0-add_integer.py module with its test case

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module provides a add_integer function.
+
+The arguments of the function are integers values and the result is its sum
+"""
+def add_integer(a, b=98):
+    """
+    This is a function for adds two integers
+
+    Args:
+       param1 (a): first number
+       param2 (b): second number
+
+    Raises:
+       TypeError: the arguments must be integers or floats
+
+    Returns:
+       The sum of the two numbers
+
+    >>> add_integer(5, 5)
+    10
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    else:
+        a = int(a)
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        b = int(b)
+
+    return (a + b)
