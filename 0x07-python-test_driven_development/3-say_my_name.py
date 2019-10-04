4ddc50b8b30ad0ad7bfe35cd1e7904c756223292
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-03 21:51:09 -0500

    [fix] pep8 style

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,7 @@ This module provides a say_my_name function
 The function works by printing the name of the user
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
     This is a function that prints a name
