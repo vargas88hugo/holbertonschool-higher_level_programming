ea03a779cf74705a7ddd6495c26e37b56b30d382
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-13 11:10:06 -0500

    [fix] missing case when the argument is not a number

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -61,6 +61,12 @@ if __name__ == "__main__":
         print("Usage: nqueens N")
         sys.exit(1)
 
+    try:
+        n = int(sys.argv[1])
+    except:
+        print("N must be a number")
+        sys.exit(1)
+
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
