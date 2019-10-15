db065e447ecdb435d0efbd8fd393d46388547638
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-15 17:24:48 -0500

    [feature] created 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module provides Square Class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square Class
+    """
+    def __init__(self, size):
+        """
+        Constructor of the class
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
