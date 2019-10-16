2eeeef3cb8099e73228c9721cbfaf08925d9af37
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-16 11:54:53 -0500

    [feature] created 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module provides read_file() function
+"""
+
+
+def read_file(filename=""):
+    """
+    This is a function that reads a text file and prints it
+
+    Args:
+        param1 (filename): the file to be read and print
+    """
+    with open(filename, encoding="utf-8") as f:
+        text = f.read()
+        print(text)
