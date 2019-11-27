fed810a10993e004062a8d7e1801b8b298ba6a65
Hugo Vargas <vargas88hugo@gmail.com>
2019-11-26 23:31:11 -0500

    [feature] created 14-my_genres.sql

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,2 @@
+-- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+SELECT tv_genres.name FROM tv_genres, tv_shows, tv_show_genres WHERE tv_genres.id = tv_show_genres.genre_id AND tv_show_genres.show_id = tv_shows.id AND tv_shows.title = "Dexter" ORDER BY name ASC;
