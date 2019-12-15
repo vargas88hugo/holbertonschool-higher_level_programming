d3ea109d3f3368499ea74bd37e1108c049fd46cb
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-15 18:31:06 -0500

    [Feature] Created 7-model_state_fetch_all.py

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script lists all states
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.engine import url
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    j = 1
+    for i in session.query(State).all():
+        print(str(j) + ": " + i.name)
+        j += 1
