dac6a58a64178e4325e035c2a3fd9662c7973d5a
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-15 19:13:37 -0500

    [Feature] created 9-model_state_filter_a.py

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script lists all states
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.engine import url
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).all()
+
+    for i in state:
+        if 'a' in i.name:
+            print(str(i.id) + ": " + i.name)
+
+    session.close()
