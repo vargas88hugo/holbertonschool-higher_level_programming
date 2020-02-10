691c37f5fc94b93fa276b28372786e544f321d3c
Hugo Vargas <vargas88hugo@gmail.com>
2020-02-10 18:35:41 -0500

    [Feature] Created 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/** Program that implements Square Class */
+const SuperSquare = require('./5-square');
+
+class Square extends SuperSquare {
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 1; i <= this.height; i++) {
+        for (let j = 1; j <= this.width; j++) {
+          process.stdout.write('C');
+        }
+        console.log();
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+
+module.exports = Square;
