f996eb7b0c4200122074ce3eb4e719c589ca41f2
Hugo Vargas <vargas88hugo@gmail.com>
2020-02-11 15:29:56 -0500

    [Feature] Created 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,17 +2,16 @@
 /** Script that prints the title of a Star War movie */
 const request = require('request');
 const url = process.argv[2];
-// const url = `http://swapi.co/api/films/`;
 
 request.get(url, (err, res, body) => {
   if (err) {
     return console.log(err);
   }
   let count = 0;
-  for (let i = 0; i < JSON.parse(body).count; i++) {
-    if (JSON.parse(body).results[i].characters.includes('https://swapi.co/api/people/18/')) {
+  const content = JSON.parse(body);
+  for (let i = 0; i < content.count; i++) {
+    if (content.results[i].characters.includes('https://swapi.co/api/people/18/')) {
       count++;
-      // console.log(JSON.parse(body).results[i].characters);
     }
   }
   console.log(count);
