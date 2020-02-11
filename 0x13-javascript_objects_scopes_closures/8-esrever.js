bc6e59bd384e3f68a6a14aa26f7ee604d4f55c9c
Hugo Vargas <vargas88hugo@gmail.com>
2020-02-10 19:01:25 -0500

    [Feature] Created 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  for (let i = 0; i <= list.length / 2; i++) {
+  for (let i = 0; i < list.length / 2; i++) {
     [list[i], list[list.length - 1 - i]] = [list[list.length - 1 - i], list[i]];
   }
 
