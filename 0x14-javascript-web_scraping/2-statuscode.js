1e5b5f33a1eb2b36f7dc53fa5e645d34f9e62776
Hugo Vargas <vargas88hugo@gmail.com>
2020-02-11 12:14:21 -0500

    [Feature] Created 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/** Script that display the status of a GET */
+const request = require('request');
+const url = process.argv[2];
+
+request(url)
+  .on('response', function (response) {
+    console.log(response.statusCode);
+  });
