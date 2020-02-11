b53b839db72b917887af2db99f1b58b9449fed7d
Hugo Vargas <vargas88hugo@gmail.com>
2020-02-11 16:04:40 -0500

    [Feature] Created 5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/** Script that gets the content of a webpage and stores it in a file */
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const path = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  fs.writeFile(path, body, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
+  console.log(body);
+});
